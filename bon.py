97bd28cde27f1a924a87b195a0f0187d5842edcc
Evgeniy-code <77773058+Evgeniy-code@users.noreply.github.com>
2021-08-08 18:24:47 +0300

    init

--- bon.py
+++
@@ -1,2 +0,0 @@
-cheslo = int(input())
-print(str(bin(cheslo))[2:], str(oct(cheslo))[2:], str(hex(cheslo))[2:].upper(), sep='\n')
--- prob.py
+++
@@ -1,9 +0,0 @@
-row = input().split()
-s = ''.join(row)
-my_list, temp_list = [[]], []
-for elem in range(len(row)):
-    temp_list.append(row[elem])
-    my_list.append(temp_list)
-    temp_list = []
-print(s)
-print(row)
