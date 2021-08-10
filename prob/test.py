e5ef97c3daaa508f0ba6758cae78f3ad2fc39687
top <pochtaEA@outlook.com>
2021-08-11 01:07:13 +0300

    def pascal

--- prob/test.py
+++ prob/test.py
@@ -1,11 +1,15 @@
+def pascal(raw):
+    for i in range(raw + 1):
+        temp_list = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                temp_list.append(1)
+            else:
+                temp_list.append(my_list[i - 1][j - 1] + my_list[i - 1][j])
+        my_list.append(temp_list)
+    return print(my_list[raw])
+
+
 n = int(input())
 my_list = []
-for i in range(n + 1):
-    temp_list = []
-    for j in range(i + 1):
-        if j == 0 or j == i:
-            temp_list.append(1)
-        else:
-            temp_list.append(my_list[i - 1][j - 1] + my_list[i - 1][j])
-    my_list.append(temp_list)
-print(my_list[n])
+pascal(n)
